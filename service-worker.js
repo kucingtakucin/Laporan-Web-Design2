9e74e6db850923eae3da59e073c3d7dd85c5f728
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-04-04 10:05:01 +0700

    Semoga ini bisa display :(

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7606e3687a50b5d5832c53d72dba52dc.js"
+  "/Laporan-Web-Design2/precache-manifest.c6ebb58dc9b99a26aca85a7fcc6434fd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Laporan-Web-Design2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
